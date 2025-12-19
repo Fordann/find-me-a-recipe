289ecf5546bb8a98776805f46406c64117222f8e
Thibault nieuviarts <thibault.nieuviarts@gmail.com>
2025-12-19 15:04:12 -0600

    fix: display glitch when fewer than 5 recipes remain: previous top deck card briefly appears on top before disappearing

--- client/src/components/RecipeFetcher.tsx
+++ client/src/components/RecipeFetcher.tsx
@@ -12,29 +12,32 @@ const RecipeFetcher: React.FC<RecipeFetcherProps> = ({ ingredientChosenByUser })
     const [recipes, setRecipes] = React.useState<Array<RecipePreview>>([]);
     const { getRecipesSummaryFromIngredient } = useRecipes();
     const [currentIndex, setCurrentIndex] = useState<number>(0);
-
-    const isEnoughRecipesFetched = recipes.length - currentIndex > 0;
+    
+    const isEnoughRecipesFetchedForDisplay = recipes.length - currentIndex > 2;
+    const needMoreRecipes = recipes.length - currentIndex < 5;
 
     useEffect(() => {
         if (!ingredientChosenByUser) return; 
 
         const fetchRecipes = async () => {
+            console.log("index before fetched: ", currentIndex);
             const new_recipes = await getRecipesSummaryFromIngredient(ingredientChosenByUser);
-            setRecipes(recipes.slice(currentIndex).concat(new_recipes));
+            console.log("index after fetched:", currentIndex);
+            setRecipes(prev => prev.slice(currentIndex).concat(new_recipes));
+            setCurrentIndex(0);
         };
         
-        if (recipes.length - currentIndex <= 5) {
-            fetchRecipes();
-            setCurrentIndex(0);
+        if (needMoreRecipes) {
+            fetchRecipes();  
         }
         
     }, [ingredientChosenByUser, currentIndex, getRecipesSummaryFromIngredient]); 
     
     return (
         <>    
-        <Fridge isLoading={ingredientChosenByUser != "" && !isEnoughRecipesFetched} />
+        <Fridge isLoading={ingredientChosenByUser != "" && !isEnoughRecipesFetchedForDisplay} />
         {   
-            ingredientChosenByUser && isEnoughRecipesFetched ?  (
+            ingredientChosenByUser && isEnoughRecipesFetchedForDisplay && 
                 <div className="search mouse-hover container">
                     <>
                         <div className="swipe-container">
@@ -44,29 +47,27 @@ const RecipeFetcher: React.FC<RecipeFetcherProps> = ({ ingredientChosenByUser })
                                     const recipeName = recipe.title;
                                     const recipeImage = recipe.image;
 
-                                    return <SwipeCard
-                                                key={`${recipeName}`}
-                                                recipe={{ title: recipeName, img: recipeImage, id: `${recipeName}` }}
-                                                    onSwipeLeft={() => {
-                                                    setCurrentIndex(prev => prev + 1)
-                                                    console.log('Recipe discarded');
-                                                }}
-                                                onSwipeRight={() => {
-                                                    console.log('Recipe added to Favorites');
-                                                    setCurrentIndex(prev => prev + 1);
-                                                }}
-                                                zIndex={2 - offset}
-                                                isTop={2 - offset === 2}
-                                            />
+                                    return (
+                                        <SwipeCard
+                                            key={`${recipeName}`}
+                                            recipe={{ title: recipeName, img: recipeImage, id: `${recipeName}` }}
+                                                onSwipeLeft={() => {
+                                                setCurrentIndex(prev => prev + 1)
+                                                console.log('Recipe discarded');
+                                            }}
+                                            onSwipeRight={() => {
+                                                console.log('Recipe added to Favorites');
+                                                setCurrentIndex(prev => prev + 1);
+                                            }}
+                                            zIndex={2 - offset}
+                                            isTop={!offset}
+                                        />
+                                    )
                                 })
-                            }
-                            
+                            }  
                         </div>
                     </>
                 </div>
-            ): ingredientChosenByUser && !isEnoughRecipesFetched ? (
-                <p>Searching For Recipes</p>
-            ) : null
         }
         <p>Recipe Fetcher Component</p>
         </>
